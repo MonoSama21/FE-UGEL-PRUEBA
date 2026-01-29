9ee2890e4933e9d1dbabe356b18974f955457cfd
MonoSama21 <yrvin21pachas2004@gmail.com>
2026-01-29 16:47:44 -0500

    Mandando cambios

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,22 +1,13 @@
-import { Hero } from '../sections/Hero'
-import { WeddingDetails } from '../sections/WeddingDetails'
-import { OurStory } from '../sections/OurStory'
-import { Gallery } from '../sections/Gallery'
-import { RSVPForm } from '../sections/RSVPForm'
 import { Footer } from '../sections/Footer'
-import { CountDown } from '../sections/CountDown'
-import { Gifts } from '../sections/Gifts'
+import { MonitoreoForm } from '../sections/MonitoreoForm'
+import { Navbar } from '../sections/Navbar'
+
 
 export const Home = () => {
   return (
     <div className="overflow-hidden">
-      <Hero />
-      <CountDown  />  
-      <WeddingDetails />
-      <OurStory />
-      <Gallery />
-      <Gifts />
-      <RSVPForm />
+      <Navbar />
+      <MonitoreoForm />
       <Footer />
     </div>
   )
